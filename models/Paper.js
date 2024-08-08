f38a2d5e13d636177519b3921e4cfbb32ad8a872
Jinit07_ <jinitjasani04@gmail.com>
2024-08-08 12:30:49 +0530

    name

--- models/Paper.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PaperSchema = new mongoose.Schema({
-    hash: { type: String, required: true },
-    professor: { type: String, required: true },
-    approved: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model('Paper', PaperSchema);
