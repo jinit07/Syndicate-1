8aae36db04628226c6e564f883abb4436d8fb654
Jinit07_ <jinitjasani04@gmail.com>
2024-08-08 12:24:34 +0530

    Rm=

--- migrations/2_diploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const Syndicate = artifacts.require("Syndicate");
-
-module.exports = function (deployer) {
-  deployer.deploy(Syndicate)
-    .then(() => console.log("Syndicate deployed successfully"))
-    .catch(error => console.error("Deployment failed:", error));
-};
